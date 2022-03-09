e97dfb8ce989a51ec709f2b51301fec5302aed9f
barrieu <37584427+barrieu@users.noreply.github.com>
2022-03-09 10:46:32 +0000

    Update setupAudio.js

--- setupAudio.js
+++ setupAudio.js
@@ -1,4 +1,4 @@
-
+console.log("setting up audiocontext");
 const audioContext = new AudioContext();
 const element = document.querySelector("audio");
 const source = audioContext.createMediaElementSource(element);
