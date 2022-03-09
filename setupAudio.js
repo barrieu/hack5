8beadde3bbe929efe233c909a08e8d2bccf84714
barrieu <37584427+barrieu@users.noreply.github.com>
2022-03-09 10:44:54 +0000

    put setupaudio back

--- setupAudio.js
+++ setupAudio.js
@@ -1,7 +1,5 @@
 
-<audio src="oodlesOfSounds.ogg"></audio>
 const audioContext = new AudioContext();
-const element = document.querySelector(audio);
+const element = document.querySelector("audio");
 const source = audioContext.createMediaElementSource(element);
-source.connect(audioContext.destination)
-audio.play();
+source.connect(audioContext.destination);
