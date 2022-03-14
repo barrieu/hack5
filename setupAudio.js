5cc10cf6a8932fc720275ee61e555dbd8021d315
barrieu <37584427+barrieu@users.noreply.github.com>
2022-03-14 15:22:51 +0000

    removed all the clutter

--- setupAudio.js
+++
@@ -1,7 +0,0 @@
-function setupAudio(){
-  console.log("setting up audiocontext");
-  const audioContext = new AudioContext();
-  const element = document.querySelector("audio");
-  const source = audioContext.createMediaElementSource(element);
-  source.connect(audioContext.destination);
-}
